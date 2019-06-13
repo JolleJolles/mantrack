90834006f71ca216965f684d737f0c2ab55612fd
joljols <j.w.jolles@gmail.com>
2019-06-13 13:48:56 +0200

    created init

---
+++ mantrack/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+################################################
+# This is the stand alone version of Mantrack, #
+# a tool from the AnimTrack package by J.W.    #
+# Jolles. Do not distribute!                   #
+################################################
+
+from .__version__ import __version__
+from .mantrack import *
